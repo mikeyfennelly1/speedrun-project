24f456d970a86a288fdee2367c4301e8512504cd
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2025-03-19 17:12:46 +0000

    SysinfoHandler created, usesless atp

--- src/main/java/com/mfennelly/SysinfoHandler.java
+++ src/main/java/com/mfennelly/SysinfoHandler.java
@@ -1,4 +1,17 @@
 package com.mfennelly;
 
-public class SysinfoHandler {
+import java.net.Socket;
+
+public class SysinfoHandler implements Runnable {
+
+    private Socket clientSocket;
+
+    public SysinfoHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("SysinfoHandler here, client address is: " + this.clientSocket.getInetAddress());
+    }
 }
